9b2bb0a92d62406868646f04f9bf9ddf9f46c2cf
Calvin Keats <calvin.keats@gmail.com>
2023-09-29 09:20:21 -0500

    images cont

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-import './App.css'
-import { ImageList, ImageListItem } from '@mui/material'
-import joker from '../src/assets/jokers/joker.png'
+// Create a type or interface for your images
+type Image = {
+  default: string;
+};
+
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import * as images from '../src/assets/jokers/index';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageComponents, setImageComponents] = useState<JSX.Element[]>([]);
   
-  const itemData = [];
+  console.info(Object.values(images));
+
+  useEffect(() => {
+    // Extract image imports from the 'images' object
+    const importedImages = Object.values(images) as Image[];
+
+    // Create image components from the imported images
+    const imageComponents = importedImages.map((image, index) => (
+      <img
+        key={index}
+        src={image.default} // Use the 'default' property to get the image URL
+        alt={`Image ${index}`}
+      />
+    ));
+
+    // Update the state with the image components
+    setImageComponents(imageComponents);
+  }, []);
+  
 
   return (
-    <>
-      <img src={joker}></img>
-    </>
-  )
+    <div className="App">
+      {imageComponents}
+    </div>
+  );
 }
 
-export default App
+export default App;
